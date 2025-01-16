abd42f62ed767cac66c46e84fa53436689709413
SayuriCristina <sayuri.cristinass@gmail.com>
2025-01-15 22:23:15 -0300

    Adiciona o board com as tarefas

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,63 @@
 import './Home.css'
-import { FaPlus } from "react-icons/fa6";
+
+const lists = [
+    {
+        id: 'todo',
+        title: 'A Fazer',
+        cards: [
+            { id: '1', titulo: 'Primeira tarefa', descricao: 'Primeira tarefa' },
+            { id: '2', titulo: 'Segunda tarefa', descricao: 'Primeira tarefa' },
+        ]
+    },
+    {
+        id: 'doing',
+        title: 'Em Andamento',
+        cards: [
+            { id: '3', titulo: 'Tarefa em progresso', descricao: 'Primeira tarefa' },
+        ]
+    },
+    {
+        id: 'done',
+        title: 'Concluído',
+        cards: [
+            { id: '4', titulo: 'Tarefa finalizada', descricao: 'Primeira tarefa' },
+        ]
+    }
+];
 
 function Home() {
+
     return (
-        <div>
+        <div className="trello-board">
+            <div className="lists-container">
+                {lists.map(list => (
+                    <div key={list.id} className="list">
+                        <h2>{list.title}</h2>
+                        <div className="cards-container">
+                            {list.cards.map(card => (
+                                <div key={card.id} className="card">
+                                    <div className='task'>
+                                        <div id='titulo'>
+                                            {card.titulo}
+                                        </div>
+                                        <div id='descricao'>
+                                            {card.descricao}
+                                        </div>
+                                    </div>
+                                    <button className="button-3d square">
+                                        <span className="text">Botão</span>
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                        <button className="text-3d">
+                            ADICIONAR CARD
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div >
+    );
+};
 
-            <button className="button-3d square" role="button">
-                <span className="text">Botão</span>
-            </button>
-            <br/>
-            <br/>
-            <br/>
-            <button className="button-3d round" role="button">
-                <FaPlus size={20} />
-            </button>
-            <h1 className="text-3d">ADICIONAR CARD</h1>
-        </div>
-
-    )
-}
-
-export default Home
+export default Home;
